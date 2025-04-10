22e3f89d215ad1d5d7adb234bccd4848d2fb71a1
Quinton LaBorde <quintonlaborde@gmail.com>
2025-04-09 18:19:21 -0600

    updated footer for light mode

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,14 @@
 const Footer = () => {
   return (
     <footer className="w-full text-center py-2 mt-12 text-sm text-gray-400 sticky top-[100vh]">
-      <p className="text-sm text-gray-400">
-        🔮 <span className="font-semibold text-white">The Oracle</span>{" "}
-        &nbsp;•&nbsp; Built by{" "}
+      <p className="text-sm text-gray-700 dark:text-gray-300">
+        🔮 <span className="font-semibold">The Oracle</span> &nbsp;•&nbsp; Built
+        by{" "}
         <a
           href="https://github.com/QuintonEL"
           target="_blank"
           rel="noopener noreferrer"
-          className="underline hover:text-purple-400"
+          className="underline hover:text-purple-500 transition"
         >
           Quinton
         </a>{" "}
